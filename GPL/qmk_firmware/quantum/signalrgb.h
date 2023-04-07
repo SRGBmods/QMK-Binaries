bcaa07cfe72b225623b293bdd46b1f361abed6e7
Naitoshedo <hozuki@naitoshedo.tv>
2023-04-07 14:03:50 -0500

    Update latest QMK Version

--- GPL/qmk_firmware/quantum/signalrgb.h
+++ GPL/qmk_firmware/quantum/signalrgb.h
@@ -19,7 +19,7 @@ enum signalrgb_responses //These are a bit clunky right now. Could use improveme
     PROTOCOL_VERSION_BYTE_3 = 4,
     QMK_VERSION_BYTE_1 = 0,
     QMK_VERSION_BYTE_2 = 20,
-    QMK_VERSION_BYTE_3 = 4,
+    QMK_VERSION_BYTE_3 = 5,
     DEVICE_UNIQUE_IDENTIFIER_BYTE_1 = 0,
     DEVICE_UNIQUE_IDENTIFIER_BYTE_2 = 0,
     DEVICE_UNIQUE_IDENTIFIER_BYTE_3 = 0,
