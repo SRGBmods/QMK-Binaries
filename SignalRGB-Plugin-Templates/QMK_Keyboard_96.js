c95d82646ef3d290acc6cc24c23c3ce539337508
Naitoshedo <hozuki@naitoshedo.tv>
2024-08-10 23:54:16 -0500

    Add Desktop VIA.exe to conflicting processes

--- SignalRGB-Plugin-Templates/QMK_Keyboard_96.js
+++ SignalRGB-Plugin-Templates/QMK_Keyboard_96.js
@@ -4,7 +4,8 @@ export function VendorId() { return 0x0000; }
 export function ProductId() { return 0x0000; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return "qmk/srgbmods-qmk-firmware"; }
-export function DeviceType() { return "keyboard";}
+export function DeviceType() { return "keyboard"; }
+export function ConflictingProcesses() { return ["VIA.exe"]; }
 export function Size() { return [19, 6]; }
 export function DefaultPosition(){return [10, 100]; }
 export function DefaultScale(){return 8.0;}
