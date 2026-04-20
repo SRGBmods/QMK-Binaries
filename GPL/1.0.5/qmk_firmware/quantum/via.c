1c88231fa228c053c8f255661a2f3b99a952b7ef
Naitoshedo <hozuki@naitoshedo.tv>
2026-04-20 17:21:27 -0500

    Update via.c
    LED indicators if defined respect the toggle and stop updating specific key's LED.

--- GPL/1.0.5/qmk_firmware/quantum/via.c
+++ GPL/1.0.5/qmk_firmware/quantum/via.c
@@ -52,7 +52,7 @@ void led_streaming(uint8_t *data) //Stream data from HID Packets to Keyboard.
 
     if(numberofleds >= 10)
     {
-        packet[1] = DEVICE_ERROR_LED_BOUNDS;
+        packet[1] = DEVICE_ERROR_LED_COUNT;
         raw_hid_send(packet,32);
         return; 
     } 
@@ -64,18 +64,45 @@ void led_streaming(uint8_t *data) //Stream data from HID Packets to Keyboard.
       uint8_t  g = data[offset + 1];
       uint8_t  b = data[offset + 2];
 
-      //if ( ((index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock) || ((index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock) || ((index + i) == SCROLL_LOCK_INDEX && host_keyboard_led_state().scroll_lock))   {
-      //if ( ((index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock) || ((index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock))   {
-      //if ( (index + i) == CAPS_MAC_WIN_INDEX && host_keyboard_led_state().caps_lock)   {
-      //if ( (index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock)   {
-      //if ( (index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock)  {
-      //#if defined(RGBLIGHT_ENABLE)
-      //rgblight_setrgb_at(255, 255, 255, index + i);
-      //#elif defined(RGB_MATRIX_ENABLE)
-      //rgb_matrix_set_color(index + i, 255, 255, 255);
-      //#endif
+      bool isIndicator = false;
 
-      //} else {
+      #if defined(CAPS_MAC_WIN_LED_INDEX)
+      if ( (index + i) == CAPS_MAC_WIN_LED_INDEX && host_keyboard_led_state().caps_lock ) isIndicator = true;
+      #endif
+
+      #if defined(NUM_LOCK_LED_INDEX)
+      if ( (index + i) == NUM_LOCK_LED_INDEX && host_keyboard_led_state().num_lock ) isIndicator = true;
+      #endif
+
+      #if defined(NUM_LOCK_INDEX)
+      if ( (index + i) == NUM_LOCK_INDEX && host_keyboard_led_state().num_lock ) isIndicator = true;
+      #endif
+
+      #if defined(CAPS_LOCK_LED_INDEX)
+      if ( (index + i) == CAPS_LOCK_LED_INDEX && host_keyboard_led_state().caps_lock ) isIndicator = true;
+      #endif
+
+      #if defined(CAPS_LOCK_INDEX)
+      if ( (index + i) == CAPS_LOCK_INDEX && host_keyboard_led_state().caps_lock ) isIndicator = true;
+      #endif
+
+      #if defined(SCROLL_LOCK_LED_INDEX)
+      if ( (index + i) == SCROLL_LOCK_LED_INDEX && host_keyboard_led_state().scroll_lock ) isIndicator = true;
+      #endif
+
+      #if defined(SCROLL_LOCK_INDEX)
+      if ( (index + i) == SCROLL_LOCK_INDEX && host_keyboard_led_state().scroll_lock ) isIndicator = true;
+      #endif
+
+      if (isIndicator) {
+
+      #if defined(RGBLIGHT_ENABLE)
+      rgblight_setrgb_at(255, 255, 255, index + i);
+      #elif defined(RGB_MATRIX_ENABLE)
+      rgb_matrix_set_color(index + i, 255, 255, 255);
+      #endif
+
+      } else {
 
       #if defined(RGBLIGHT_ENABLE)
       rgblight_setrgb_at(r, g, b, index + i);
@@ -84,7 +111,7 @@ void led_streaming(uint8_t *data) //Stream data from HID Packets to Keyboard.
       #endif
         }
      }
-//}
+}
 
 void signalrgb_mode_enable(void)
 {
